6fee4e9cf2ff65c4de1cf8aba5d6472ce8d5e18b
Stipo Margic <stipo@liberato.io>
2020-05-30 15:58:20 +0200

    Added calculator

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 const toggle = document.querySelector('#toggle'),
-  year = document.querySelector('#year');
+  year = document.querySelector('#year'),
+  moneyInput = document.querySelector('#money'),
+  monthsInput = document.querySelector('#months'),
+  calculateBtn = document.querySelector('#calculate'),
+  moneyOutput = document.querySelector('#pmoney'),
+  monthsOutput = document.querySelector('#pmonths'),
+  payback = document.querySelector('#payback');
 
 year.innerText = new Date().getFullYear();
+
+const calculatePayback = (input) => {
+  return +input * 0.3 + +input;
+};
 
 // Toggle nav
 toggle.addEventListener('click', () =>
   document.body.classList.toggle('show-nav')
 );
+
+calculateBtn.addEventListener('click', () => {
+  moneyOutput.innerText = moneyInput.value;
+  monthsOutput.innerText = monthsInput.value;
+
+  payback.innerText = calculatePayback(moneyInput.value);
+});
